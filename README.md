c9b020b01c32f96c07899c9498b4c70896c59369
awgibson <34604592+awgibson@users.noreply.github.com>
2019-04-14 15:46:38 -0500

    finalized project

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Treehouse-Tech-Degree-Project-8
+
+## Goal
+In this project you’ll be provided a website with HTML, SCSS, JPEGs, PNGs and JavaScript files. You’ll be required to set up a Gulp build process to prepare the website for deployment.
+
+### The build process must fulfill the following criteria:
+
+1. Concatenate and minify the JavaScript files
+2. Compile SCSS into CSS in a concatenated and minified file
+3. Generate JavaScript and CSS source maps
+4. Compress any JPEG or PNG files
+5. All output for the build process should be in a dist folder for distribution or deployment.
+
+***
+
+## Grading Criteria
++ Running the npm install command installs the build process dependencies properly
++ The gulp scripts command concatenates, minifies, and copies all of the project’s JavaScript files into an all.min.js file
++ The command copies the all.min.js file into the dist/scripts folder
++ The gulp styles command compiles the project’s SCSS files into CSS, and concatenates and minifies into an all.min.css file
++ The command copies the all.min.css file into the dist/styles folder
++ The gulp scripts command generates JavaScript source maps
++ The gulp styles command generates CSS source maps
++ The gulp images command copies the optimized images to the dist/content folder.
++ The gulp clean command deletes all of the files and folders in the dist folder.
++ The gulp build command properly runs the clean, scripts, styles, and images tasks.
++ The clean task fully completes before the scripts, styles, and images tasks are ran.
++ The gulp command properly runs the build task as a dependency
++ The gulp command serves the project using a local webserver.
++ **Exceeds**: The gulp command also listens for changes to any .scss file. When there is a change to any .scss file, the gulp styles command is run, the files are compiled, concatenated and minified to the dist folder, and the browser reloads, displaying the changes
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
-//Bring in dependencies
+////////////////////////////
+// Bring in dependencies //
+//////////////////////////
+
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
@@ -7,10 +10,14 @@ const rename = require('gulp-rename');
 const image = require('gulp-image');
 const del = require('del');
 const webserver = require('gulp-webserver');
-const inject = require('gulp-inject');
 
-//File paths
+/////////////////
+// File paths //
+///////////////
+
+//----Root directory
 const distRoot = './dist';
+
 //----Javascript
 const jsRoot = './js';
 const jsDist = `${distRoot}/scripts`;
@@ -19,63 +26,80 @@ const jsFiles = [
   `${jsRoot}/circle/circle.js`,
   `${jsRoot}/global.js`
 ];
+
 //----Sass
 const sassRoot = './sass';
 const sassDist = `${distRoot}/styles`;
 const sassFiles = `${sassRoot}/**/*.scss`;
+
 //----Images
 const imgRoot = './images';
 const imgDist = './dist/content';
 const imgFiles = `${imgRoot}/*`;
+
 //----HTML and Other Static Resources
 const htmlFiles = './*.html';
 const iconsFiles = './icons/**';
 
+///////////////
+//  Tasks  //
+////////////
+
+//----Handles minification of javascript files
 function scripts() {
   return gulp
-    .src(jsFiles, { sourcemaps: true })
-    .pipe(concat('all.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(jsDist, { sourcemaps: '.' }));
+    .src(jsFiles, { sourcemaps: true }) // targets javascript files and uses built in sourcemapping
+    .pipe(concat('all.min.js')) // concats javascript to all.min.js
+    .pipe(uglify()) // uglify the code
+    .pipe(gulp.dest(jsDist, { sourcemaps: '.' })); // save file to the dist folder with a seperate sourcemap file
 }
 
+//----Handles compiling sass files
 function styles() {
   return gulp
-    .src(sassFiles, { sourcemaps: true })
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(rename('all-min-css.css'))
-    .pipe(gulp.dest(sassDist, { sourcemaps: '.' }));
+    .src(sassFiles, { sourcemaps: true }) // targets sass files and uses built in sourcemapping
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // converts sass to a single css file and minifies at the same time
+    .pipe(rename('all.min.css.css')) // rename file to all-min-css
+    .pipe(gulp.dest(sassDist, { sourcemaps: '.' })); // save file to dist folder with sourcemaps saved as a seperate file
 }
 
+//----Handles optimizing images
 function images() {
   return gulp
-    .src(imgFiles)
-    .pipe(image())
-    .pipe(gulp.dest(imgDist));
+    .src(imgFiles) // targets images
+    .pipe(image()) // sends images to the image dependency
+    .pipe(gulp.dest(imgDist)); // outputs to the dist folder
 }
 
+//----Handles copying static files
 function copyStatic() {
-  return gulp.src([htmlFiles, iconsFiles], { base: './' }).pipe(gulp.dest(distRoot));
+  return gulp.src([htmlFiles, iconsFiles], { base: './' }).pipe(gulp.dest(distRoot)); // targets static files, keeps folder structure, copies to dist folder
 }
 
+//----Deletes all files in dist folder
 function clean() {
   return del(`${distRoot}/*`);
 }
 
+//----Sets dev webserver
 function server() {
+  // points server to the dist folder
   return gulp.src(distRoot).pipe(
     webserver({
-      livereload: true,
-      open: true
+      livereload: true, // enables live reload on file changes
+      open: true // opens browser on load
     })
   );
 }
 
+//----Watches for file changes
 function watch() {
-  gulp.watch(sassFiles, styles);
+  gulp.watch(sassFiles, styles); // Watches for sass file changes then starts the styles function
 }
 
-const build = gulp.series([clean, gulp.parallel([scripts, styles, images, copyStatic])]);
+////////////////////////
+//  Exporting Tasks  //
+//////////////////////
 
 exports.scripts = scripts;
 exports.styles = styles;
@@ -83,5 +107,10 @@ exports.images = images;
 exports.clean = clean;
 exports.watch = watch;
 
+// Variable to hold the build order for reuse
+// Clean task is run first then the rest of the tasks are allowed to run in parallel
+// since they do not depend on one another to run
+const build = gulp.series([clean, gulp.parallel([scripts, styles, images, copyStatic])]);
+
 exports.build = build;
-exports.default = gulp.series([build, server, watch]);
+exports.default = gulp.series([build, server, watch]); //Runs the build task then launches the server and file watch
